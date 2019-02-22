78cfda33348cf0482d7b921d6b02e9f614560eed
aymericfloyrac <aymeric.floyrac@polytechnique.edu>
2019-02-22 10:18:19 +0100

    Add files via upload

--- prediction.R
+++ prediction.R
@@ -1,6 +1,7 @@
 setwd(dir = "/Users/aymeric/Documents/ENSAE/2A/Semestre 2/Séminaire statistiques")
 df <- read.csv('modified_data4.csv',header = T)
-
+df<-select(df,-c(w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12,w13,w14,w15,w16,w17,w18,w19,w20,w21,w22,w23,w24,w25))
+df<-select(df,-c(farniente,day,Holiday,X.1,X.x,date))
 #regression linéaire
 fit<-lm(LOAD~hour+month+year+average+maxload+minload+averageload+laggedtemp+maxtemp+mintemp+averagetemp+prevload+tempdiff+weekend+notworking+heurepleine, data=df)
 summary(fit)
@@ -8,8 +9,8 @@ summary(fit)
 #regression LASSO
 library(lars)
 library(dplyr)
-x = select(df,-c(X,LOAD))
-lasso<-lars(x=,y=df$LOAD)
+x = as.matrix(select(df,-c(X,LOAD)))
+lasso<-lars(x=x,y=df$LOAD)
 plot(lasso)
 
 #regression ridge
