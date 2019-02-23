45bf9acdba031e5b2443ca3b706b0cd745012d8e
Mehdi <mehdi-bs@live.fr>
2019-02-23 13:05:02 +0100

    Update prediction.R
    Naive prediction (horaire et mensuelle)

--- prediction.R
+++ prediction.R
@@ -9,8 +9,37 @@ library(mgcv)
 setwd(dir = "/Users/aymeric/Documents/ENSAE/2A/Semestre 2/Séminaire statistiques" )
 df <-read.csv('modified_data6.csv')
 df <- df[!is.na(df$averagetemp),]
+df_naif <- df
 df<-select(df,-c(w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12,w13,w14,w15,w16,w17,w18,w19,w20,w21,w22,w23,w24,w25 ) )
 df<-select(df,-c(Holiday,date ) )
+
+########################
+#Prédictions naives à l'horizon horaire ou mensuel#
+########################
+naive_forecast <- function(data, step) {
+  if (step=="month") {
+    nf<-data[c("LOAD","date")]
+    tf<-nf[floor_date(ymd(nf$date),'month')!=ymd("2005-01-01"),]
+    pf<-nf[floor_date(ymd(nf$date),'month')!=ymd("2011-12-01"),]
+    res=data.frame(true=pf$LOAD,predicted=tf$LOAD)
+    mae=mean(abs(res$true-res$predicted))
+    mape=mean(abs(res$true-res$predicted)/res$true)
+    return(list(mae,mape))
+  }
+  if (step=="hour") {
+    nf<-data[c("LOAD","date")]
+    tf<- nf[-1,]
+    pf<-nf[1:length(nf[[1]])-1,]
+    res=data.frame(true=pf$LOAD,predicted=tf$LOAD)
+    mae=mean(abs(res$true-res$predicted))
+    mape=mean(abs(res$true-res$predicted)/res$true)
+    return(list(mae,mape))
+  }
+}
+
+naive_forecast(df,"hour")
+naive_forecast(df,"month")
+
 #regression linéaire
 fit<-lm(LOAD~hour+month+year+average+maxload+minload+averageload+laggedtemp+maxtemp+mintemp+averagetemp+prevload+tempdiff+weekend+notworking+heurepleine, data=df )
 summary(fit )
