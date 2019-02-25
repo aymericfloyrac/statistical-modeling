85d797bd12050ccb2d434e49665fa3d6359a2cda
Benane09 <ane.benoit@gmail.com>
2019-02-25 22:28:30 +0100

    Update prediction.R

--- prediction.R
+++ prediction.R
@@ -10,11 +10,40 @@ library(ggplot2)
 rm(list=ls())
 setwd(dir = "/Users/aymeric/Documents/ENSAE/2A/Semestre 2/Séminaire statistiques" )
 df <-read.csv('modified_data7.csv')
+
+###############
+###Série temp##
+###############
+#A préciser
+
+plot.ts(df$LOAD, main = "LOAD au cours du temps", xlab = "Temps", ylab = "Valeur de la consommation")
+
+#Autocorrélogramme et autocorrélogramme partiel
+acf(df$LOAD,lag.max = 15, main="Autocorrélogramme de la série", xlab="Retard")
+pacf(df$LOAD, lag.max = 15, main = "Autocorrélogramme partiel", xlab = "Retard")
+#Forte corrélationa vec le lagged 1 et lagged 2
+
+#Xt - Xt-1 (au pas horaire)
+acf(diff(df$LOAD, differences = 1))
+pacf(diff(df$LOAD, differences = 1))
+
+#Test de Dickey-Fuller : pour la stationnarité de la série temp
+adf.test(df$LOAD, alternative = "stationary", k=0)
+
+#ARIMA (pas du tout le bon modèle)
+arima(df$LOAD, order = c(2,0,0))
+
+#Arime forecats
+test.arima.200 <- arima(df$LOAD, order = c(2,0,0))
+pred <- predict(test.arima.200, n.ahead = 100)
+
+
 ######################
 #iterative prediction#
 ######################
-iterative<-function(object,dataset,forward){
-#object est le prédicteur, ds la base de données entière(train+test),forward est le nombre de pas dans le futur
+iterative<-function(object,dataset,forward,name){
+  #object est le prédicteur, ds la base de données entière(train+test),forward est le nombre de pas dans le futur, 
+  #name le nom pour le graphique exporté
   ds<-dataset
   l<-length(ds$LOAD)
   ytrue<-ds$LOAD[(l-forward+1):l]
@@ -27,16 +56,43 @@ iterative<-function(object,dataset,forward){
     features$averageload<-mean(ds$LOAD[(i-24*7):(i-1)])
     ds$LOAD[i]<-predict(object,features)
   }
-  
   ypred<-ds$LOAD[(l-forward+1):l]
-  p1 <- ggplot() + geom_line(aes(y = ypred, x = seq(1,forward), colour = 'prediction'))+
-    geom_line(aes(y = ytrue,x=seq(1,forward),colour='truth'))
+  p1 <- ggplot() + geom_line(aes(y = ypred, x = seq(1,forward), colour = 'prediction'),alpha=0.4)+
+    geom_line(aes(y = ytrue,x=seq(1,forward),colour='truth'),alpha=0.4)+
+    labs(x="Horizon temporel",y = 'Niveau consommation électricité', colour="Variable") +
+    theme_bw()
   p1
+  ggsave(paste0("pred_",name,".png"))
   result<-list(rmse=rmse(ytrue,ypred),ytrue=ytrue,ypred=ypred)
   return(result)
 }
 
+########################
+#Prédictions naives à l'horizon horaire ou mensuel#
+########################
+naive_forecast <- function(data, step) {
+  if (step=="month") {
+    nf<-data[c("LOAD","date")]
+    tf<-nf[floor_date(ymd(nf$date),'month')!=ymd("2005-01-01"),]
+    pf<-nf[floor_date(ymd(nf$date),'month')!=ymd("2011-12-01"),]
+    res=data.frame(true=pf$LOAD,predicted=tf$LOAD)
+    mae=mean(abs(res$true-res$predicted))
+    mape=mean(abs(res$true-res$predicted)/res$true)
+    return(list(mae,mape))
+  }
+  if (step=="hour") {
+    nf<-data[c("LOAD","date")]
+    tf<- nf[-1,]
+    pf<-nf[1:length(nf[[1]])-1,]
+    res=data.frame(true=pf$LOAD,predicted=tf$LOAD)
+    mae=mean(abs(res$true-res$predicted))
+    mape=mean(abs(res$true-res$predicted)/res$true)
+    return(list(mae,mape))
+  }
+}
 
+naive_forecast(df,"hour")
+naive_forecast(df,"month")
 
 #####################
 #régression linéaire#
@@ -45,10 +101,9 @@ forward<-24
 train<-df[0:-forward,]
 fit<-lm(LOAD~hour+month+year+temp+maxload+minload+averageload+laggedtemp+maxtemp+mintemp+averagetemp+prevload+tempdiff+notworking+heurepleine, data=train )
 summary(fit)
-ret<-iterative(object = fit,dataset = df,forward = forward)
-ret$rmse
-
+score<-iterative(object = fit,dataset = df,forward = forward,name = "reglin")
 stargazer(fit, title="Resultats")
+
 
 
 ########################
@@ -57,30 +112,30 @@ stargazer(fit, title="Resultats")
 
 x = as.matrix(select(df,-c(X,LOAD )))
 
-
 ##regression ridge
-
-ridge <- glmnet(x = x,y = df$LOAD,alpha = 0,family='gaussian' ) #alpha=0 donne la pénalisation ridge
 rcv <- cv.glmnet(x= x,y=df$LOAD,alpha = 0,family="gaussian",nfold=3)
 plot(rcv)
-best_ridge <- glmnet(x = x,y = df$LOAD,alpha = 0,family='gaussian',lambda = c(rcv$lambda.min))
-best_ridge$beta
+ridge <- glmnet(x = x,y = df$LOAD,alpha = 0,family='gaussian',lambda = c(rcv$lambda.min))
 plot_glmnet(ridge, label=5,main = paste("Régression Ridge", "meilleur lambda =",rcv$lambda.min),xlab="Valeurs de log(Lambda)", ylab="Valeur des coefficients")
 abline(v = log(rcv$lambda.min),col="red", lty=2)
-Beta_ridge = data.frame(as.data.frame(as.matrix(best_ridge$beta)))
+Beta_ridge = data.frame(as.data.frame(as.matrix(ridge$beta)))
 xtable(Beta_ridge)
 
 
+score<-iterative(object = ridge,dataset = df,forward = forward,name = "ridge")
+
+
 ##LASSO
-lasso <- glmnet(x = x,y = df$LOAD,alpha = 1,family='gaussian' ) #alpha=1 donne la pénalisation lasso
-rcv <- cv.glmnet(x= x,y=df$LOAD,alpha = 1,family="gaussian",nfold=3)
+rcv <- cv.glmnet(x= x,y=df$LOAD,alpha = 1,family="gaussian",nfold=3) #alpha=1 donne la pénalisation lasso
 plot(rcv)
-best_lasso <- glmnet(x = x,y = df$LOAD,alpha = 1,family='gaussian',lambda = c(rcv$lambda.min))
-best_lasso$beta
+lasso <- glmnet(x = x,y = df$LOAD,alpha = 1,family='gaussian',lambda = c(rcv$lambda.min))
 plot_glmnet(lasso, label = 5,  main = paste("Régression Lasso", "meilleur lambda =",rcv$lambda.min),xlab="Valeurs de log(Lambda)", ylab="Valeur des coefficients")
 abline(v = log(rcv$lambda.min),col="red", lty=2)
 Beta_lasso = data.frame(as.data.frame(as.matrix(best_lasso$beta)))
 xtable(Beta_lasso)
+
+score<-iterative(object = lasso,dataset = df,forward = forward,name = "lasso")
+
 
 ##########
 ##GAM#####
@@ -121,3 +176,6 @@ plot(ypred,type='l',col="red")
 lines(ytrue,col="green")
 
 stargazer(g, title="Resultats")
+
+
+
