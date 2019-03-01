d9cfe48a3467db9a16d2f7d727d73f0a6cef19ff
Benane09 <ane.benoit@gmail.com>
2019-03-01 18:52:32 +0100

    Update prediction.R

--- prediction.R
+++ prediction.R
@@ -13,24 +13,26 @@ setwd(dir = "/Users/aymeric/Documents/ENSAE/2A/Semestre 2/Séminaire statistique
 df <-read.csv('modified_data7.csv')
 
 
-
 ######################
 #iterative prediction#
 ######################
-iterative<-function(object,dataset,forward,name){
-  #object est le prédicteur, ds la base de données entière(train+test),forward est le nombre de pas dans le futur, 
-  #name le nom pour le graphique exporté
+
+iterative<-function(object,dataset,forward,name,title=''){
+  #object est le prÃ©dicteur, ds la base de donnÃ©es entiÃ¨re(train+test),forward est le nombre de pas dans le futur, 
+  #name le nom pour le graphique exportÃ©
+  if (title==''){title<-name}
   ds<-dataset
   l<-length(ds$LOAD)
   ytrue<-ds$LOAD[(l-forward+1):l]
   ds$LOAD[(l-forward+1):l]<-rep(0,forward) #on remplit la colonne de LOADs inconnue avec des 0 
   for (i in seq(l-forward+1,l)){
     features<-ds[i,]
-    features$prevload<-ds$LOAD[(i-24)]
+    features$prevload_24<-ds$LOAD[(i-24)]
+    features$prevload_1<-ds$LOAD[(i-1)]
     features$maxload<-max(ds$LOAD[(i-24):(i-1)])
     features$minload<-min(ds$LOAD[(i-24):(i-1)])
     features$averageload<-mean(ds$LOAD[(i-24*7):(i-1)])
-    if (name == "reglin"){
+    if (name == "reglin" || name=='gam'){
       ds$LOAD[i]<-predict(object,features)
     }
     if (name == "ridge"){
@@ -46,20 +48,20 @@ iterative<-function(object,dataset,forward,name){
   }
   ypred<-ds$LOAD[(l-forward+1):l]
   p1 <- ggplot() + geom_line(aes(y = ypred, x = seq(1,forward), colour = 'prédiction'),alpha=0.4)+
-    geom_line(aes(y = ytrue,x=seq(1,forward),colour='réelle'),alpha=0.4)+
+    geom_line(aes(y = ytrue,x=seq(1,forward),colour='rÃ©elle'),alpha=0.4)+
     labs(x="Horizon temporel",y = 'Niveau consommation électricité', colour="Variable") +
     theme_bw()
   p1
-  ggsave(paste0("pred_",name,".png"),plot = p1)
-  result<-list(rmse=rmse(ytrue,ypred),ytrue=ytrue,ypred=ypred)
+  ggsave(paste0("pred_",title,".png"),plot = p1)
+  result<-list(mape=mape(ytrue,ypred),ytrue=ytrue,ypred=ypred)
   return(result)
 }
 
 
 
-########################
+###################################
 #Prédictions itérative sur l'année#
-########################
+###################################
 
 nbjours <- function(date) {
   #donne le nombre de jours dans le mois correspondant à la date donnée
@@ -73,13 +75,12 @@ nbjours <- function(date) {
 }
 
 
-
-forecast_next_month <- function(predicteur,dataseti,year=2011,month_ahead=12) {
-  #Predicteur est le predicteur fitté sur le train set
+forecast_next_month <- function(predicteur,dataseti,year=2011,month_ahead=12, name) {
+  #Predicteur est le predicteur fittÃ© sur le train set
   #dataseti est le dataset en entier (y compris 2011)
-  #year donne l'année qu'on va prédire (ici la dernière du dataset)
-  #month_ahead donne le nombre de mois que l'on veut prédire (max 12 et min 1)
-  #renvoie une liste contenant les résultats de prédictions pour chaque mois
+  #year donne l'annÃ©e qu'on va prÃ©dire (ici la derniÃ¨re du dataset)
+  #month_ahead donne le nombre de mois que l'on veut prÃ©dire (max 12 et min 1)
+  #renvoie une liste contenant les rÃ©sultats de prÃ©dictions pour chaque mois
   ans <- vector("list", month_ahead)
   for (i in 1:month_ahead) {
     remove_month <- c(seq(1:i))
@@ -88,12 +89,11 @@ forecast_next_month <- function(predicteur,dataseti,year=2011,month_ahead=12) {
     year=df_cop$year[length(df_cop$year)]
     d <- ymd(paste(as.character(year),paste(paste("-",as.character(month),sep=""),"-11",sep=""),sep=""))
     nb_days = nbjours(d)
-    ans[[i]]<-iterative(object=predicteur,df_cop,nb_days*24,month.abb[month])
+    ans[[i]]<-iterative(object=predicteur,dataset = df_cop,forward = nb_days*24,name,title = month.abb[month])
   }
   return(ans)
 }
 
-pred_annuelle=forecast_next_month(g,df,year=2011,month_ahead=12)
 
 
 plot_rmse_month <- function(li) {
@@ -107,7 +107,7 @@ plot_rmse_month <- function(li) {
   p1
 }
 
-plot_pred_hour <- function(li) {
+plot_pred_hour <- function(li,name) {
   pred_heure = c(li[[1]]$ypred)
   true_heure = c(li[[1]]$ytrue)
   for (i in 2:12){
@@ -116,14 +116,20 @@ plot_pred_hour <- function(li) {
   }
   p1 <- ggplot() + geom_line(aes(y = pred_heure, x = seq(1,length(pred_heure)), colour = 'prediction'),alpha=0.5)+
     geom_line(aes(y = true_heure,x=seq(1,length(pred_heure)),colour='truth'),alpha=0.3)+
-    labs(title = "Prédictions au pas horaire sur toute l'année",x="Heure",y = 'Niveau consommation électricité', colour="Variable") +
-    theme_bw()
+    labs(title = "Prédictions au pas horaire sur l'année 2011",x="Heure",y = 'Niveau consommation électricité', colour="Variable") +
+    scale_x_continuous(breaks=c(1,745,1417,2161,2881,3625,4345,5089,5833,6553,7297,8071), labels=c("Janvier","Février", "Mars", "Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"))+
+    theme_bw()+
+    theme(axis.text.x=element_text(angle=45,hjust=1))
   p1
+  ggsave(paste0('plot_',name,'_heure.png'),p1)
 }
 
-plot_rmse_month(pred_annuelle)
-plot_pred_hour(pred_annuelle)
 
+#uniquement pour ben car mape ne marche pas sur son pc de tocard
+mape <- function(true,pred){
+  mp <- mean(abs((true-pred)/true))
+  return(mp)
+}
 
 get_errors <- function(li){
   me=c(rep(0,12))
@@ -138,18 +144,17 @@ get_errors <- function(li){
   return(res)
 }
 
-error_table<-get_errors(pred_annuelle)
 
-#pour le code latex
-xtable(error_table)
 
-########################
+###################################################
 #Prédictions naives à l'horizon horaire ou mensuel#
-########################
+###################################################
+
+
 naive_forecast <- function(data, step,year=2011) {
-  #Quand step = month, la fonction renvoie la mae et la mape des prédicitons naive
-  #détaillé pour chaque mois de l'année "year"
-  #pour step = hour, les predicitons naive sont réalisées au pas horaire et la fonction donne 
+  #Quand step = month, la fonction renvoie la mae et la mape des prÃ©dicitons naive
+  #dÃ©taillÃ© pour chaque mois de l'annÃ©e "year"
+  #pour step = hour, les predicitons naive sont rÃ©alisÃ©es au pas horaire et la fonction donne 
   #la mae et la mape moyenne sur tout le dataset.
   if (step=="month") {
     nf<-data[c("LOAD","date")]
@@ -186,6 +191,8 @@ naive_forecast(df,"month")
 ###Série temp##
 ###############
 
+#A voir avec le chargé de TD pour le tuning
+
 plot.ts(df$LOAD, main = "LOAD au cours du temps", xlab = "Temps", ylab = "Valeur de la consommation")
 #On a l'impression que c'est stationnaire
 
@@ -204,12 +211,25 @@ ytrue <- ytrue$LOAD
 arima.411 <- arima(train$LOAD, order = c(4,1,1))
 ypred <- predict(arima.411, n.ahead = forward)
 ts.plot(ytrue,ypred$pred, col =c("blue","red"), main = "Prédictions de l ARIMA")
-        
+
+#Sur 2011
+train<-df[!(df$year == 2011),]
+test <-df[(df$year == 2011),]
+ytrue <- test$LOAD
+n = length(test$LOAD)
+arima.411 <- arima(train$LOAD, order = c(4,1,1))
+ypred <- predict(arima.411, n.ahead = n)
+p1 <- ggplot() + geom_line(aes(y = ypred$pred, x = seq(1,n), colour = 'prediction'),alpha=0.5)+
+  geom_line(aes(y = ytrue,x=seq(1, n),colour='truth'),alpha=0.3)+
+  labs(title = "Prédictions au pas horaire sur l'année 2011",x="Heure",y = 'Niveau consommation électricité', colour="Variable") +
+  scale_x_continuous(breaks=c(1,745,1417,2161,2881,3625,4345,5089,5833,6553,7297,8071), labels=c("Janvier","Février", "Mars", "Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"))+
+  theme_bw()+
+  theme(axis.text.x=element_text(angle=45,hjust=1))
+p1
 
 
 
-#Si on le tune nous même
-
+#Si on le tune nous même => a demander au chargé de TD
 #Autocorrélogramme et autocorrélogramme partiel
 acf(df$LOAD,lag.max = 50, main="Autocorrélogramme de la série", xlab="Retard")
 #ce n'est clairement pas stationnaire car la décroissance vers 0 est très lente donc d = 1
@@ -231,9 +251,11 @@ ypred <- predict(arima.28115, n.ahead = forward)
 ts.plot(ytrue,ypred$pred, col =c("blue","red"), main = "Prédictions de l ARIMA")
 
 
+
 #####################
 #régression linéaire#
 #####################
+
 forward<-24
 train<-df[0:-forward,]
 fit<-lm(LOAD~hour+month+year+temp+maxload+minload+averageload+laggedtemp+maxtemp+mintemp+averagetemp+prevload+tempdiff+notworking+heurepleine, data=train )
@@ -241,6 +263,12 @@ summary(fit)
 score<-iterative(object = fit,dataset = df,forward = forward,name = "reglin")
 stargazer(fit, title="Resultats")
 
+#sur 2011
+pred_annuelle_reglin=forecast_next_month(fit,df,year=2011,month_ahead=12,name = "reglin")
+error_table_reglin <-get_errors(pred_annuelle_reglin)
+#pour le code latex
+xtable(error_table_reglin)
+plot_pred_hour(pred_annuelle_reglin,'reglin')
 
 
 ########################
@@ -253,7 +281,8 @@ train<-df[0:-forward,]
 x = as.matrix(select(train,-c(X,LOAD )))
 y = as.matrix(train$LOAD)
 
-##regression ridge
+##regression ridge##
+
 rcv <- cv.glmnet(x= x,y=y,alpha = 0,family="gaussian",nfold=3)
 plot(rcv)
 ridge <- glmnet(x = x,y = y,alpha = 0,family='gaussian',lambda = c(rcv$lambda.min))
@@ -263,10 +292,15 @@ Beta_ridge = data.frame(as.data.frame(as.matrix(ridge$beta)))
 xtable(Beta_ridge)
 
 
-score<-iterative(object = ridge,dataset = df,forward = forward,name = "ridge")
+#sur 2011
+pred_annuelle_ridge=forecast_next_month(ridge,df,year=2011,month_ahead=12,name = "ridge")
+error_table_ridge<-get_errors(pred_annuelle_ridge)
+#pour le code latex
+xtable(error_table_ridge)
+plot_pred_hour(pred_annuelle_ridge,'ridge')
 
+##régression LASSO##
 
-##LASSO
 rcv <- cv.glmnet(x= x,y=y,alpha = 1,family="gaussian",nfold=3) #alpha=1 donne la pénalisation lasso
 plot(rcv)
 lasso <- glmnet(x = x,y = y,alpha = 1,family='gaussian',lambda = c(rcv$lambda.min))
@@ -275,10 +309,16 @@ abline(v = log(rcv$lambda.min),col="red", lty=2)
 Beta_lasso = data.frame(as.data.frame(as.matrix(best_lasso$beta)))
 xtable(Beta_lasso)
 
-score<-iterative(object = lasso,dataset = df,forward = forward,name = "lasso")
+
+#sur 2011
+pred_annuelle_lasso=forecast_next_month(lasso,df,year=2011,month_ahead=12,name = "ridge")
+error_table_lasso <-get_errors(pred_annuelle_lasso)
+#pour le code latex
+xtable(error_table_lasso)
+plot_pred_hour(pred_annuelle_lasso,'lasso')
 
 
-##########
+#########
 ##GAM#####
 ##########
 
@@ -291,35 +331,28 @@ plot(model)
 stargazer(model, title="Resultats")
 
 #GAM 
-#GAM 
-g <- gam(formula = LOAD~s(hour,k=10)+s(month,k=10)+notworking+heurepleine+year+s(temp,k=10)+s(maxload,k=10)+s(minload,k=10)+s(averageload,k=10)+s(laggedtemp,k=10)+s(maxtemp,k=10)+s(mintemp,k=10)+s(averagetemp,k=10)+s(prevload,k=10)+s(tempdiff,k=10),
-         data = df,
-         fit = T,
-         family = 'gaussian',
-         select = T)
-
-summary(g)
-gam.check(g)
-
-#train test
-#index<-dim(df)[1]-30*24
-#train<-df[1:index,]
-#test <-df[index:dim(df)[1],]
-
 train<-df[!(df$year == 2011),]
 test <-df[(df$year == 2011),]
-g <- gam(formula = LOAD~s(hour,k=10)+s(month,k=10)+notworking+heurepleine+year+s(temp,k=10)+s(maxload,k=10)+s(minload,k=10)+s(averageload,k=10)+s(laggedtemp,k=10)+s(maxtemp,k=10)+s(mintemp,k=10)+s(averagetemp,k=10)+s(prevload,k=10)+s(tempdiff,k=10),
+g <- gam(formula = LOAD~s(hour,k=20)+month+notworking+heurepleine+year+s(temp,k=20)+s(maxload,k=10)+s(minload,k=10)+s(averageload,k=10)+s(laggedtemp,k=30)+s(maxtemp,k=10)+s(mintemp,k=10)+s(averagetemp,k=20)+s(prevload_1,k=40)+s(prevload_24,k=10)+s(tempdiff,k=20),
          data = train,
          fit = T,
          family = 'gaussian',
          select = T)
 
-ypred<-predict.gam(object = g,newdata = test[,3:17],type = 'lpmatrix')
-ytrue<-test[,2]
-plot(ypred,type='l',col="red")
-lines(ytrue,col="green")
+summary(g)
+plot(g,shade=T,residuals = T)
 
+#res<-iterative(object = g,dataset = df,forward = dim(test)[1],name = 'gam')
+
+#Sur 2011
+pred_annuelle_gam=forecast_next_month(g,df,year=2011,month_ahead=12,name = 'gam')
+error_table_gam <-get_errors(pred_annuelle_gam)
+#pour le code latex
+xtable(error_table_gam)
+plot_pred_hour(pred_annuelle_gam,'gam')
+gam.check(g)
 stargazer(g, title="Resultats")
+
 
 
 
